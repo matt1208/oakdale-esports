d17a1ab7e0ee0941d18d9187f9c5fa1ca686991b
Matthew Van Dyke <matthewvandyke08@gmail.com>
2021-06-11 20:52:04 -0700

    Test Run of Bars

--- pages/index.js
+++ pages/index.js
@@ -14,12 +14,14 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        {/* <div className={styles.bars}>
-          <img src="Group 1.png"></img>
-        </div> */}
+         <div className={styles.bars}></div> 
+         <div className={styles.barsYellow}></div>
         <h1 className={styles.title}>
           Oakdale High<br></br><span className={styles.accentText}>Esports</span>
         </h1>
+
+        <div className={styles.barsrightYellow}></div> 
+        <div className={styles.barsright}></div> 
 
         <div className={styles.specialText}>
           {/* <p>Coming Soon</p> */}
