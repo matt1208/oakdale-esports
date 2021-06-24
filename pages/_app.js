53a5db77bf411aaf299d66165497dd5d4e22a383
Matthew <matthewvandyke08@gmail.com>
2021-06-24 12:11:10 -0700

    Fix SEO

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { Fonts } from '../styles/Fonts'
 import '../styles/globals.css'
 import { DefaultSeo } from 'next-seo';
+import SEO from 'next-seo-config';
 
 
 function MyApp({ Component, pageProps }) {
