fc1e3718d5c74ef8dd1fd97c971bb69120261722
Matthew <matthewvandyke08@gmail.com>
2021-08-12 18:09:19 -0700

    Rough Home Page

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import { Fonts } from '../styles/Fonts'
 import '../styles/globals.css'
 import { DefaultSeo } from 'next-seo';
 import SEO from '/next-seo-config';
+import { ChakraProvider } from "@chakra-ui/react"
+
 
 function MyApp({ Component, pageProps }) {
   return (
