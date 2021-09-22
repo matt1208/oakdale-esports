0fd52c882f8d35b9210f730bc2add357f1756110
Matthew Van Dyke <matthewvandyke08@gmail.com>
2021-09-22 11:04:56 -0700

    Schedule Board Resposnive Overhaul

--- components/scheduleBoard.js
+++ components/scheduleBoard.js
@@ -2,33 +2,12 @@ import styles from '../styles/Home.module.css'
 import Icon from 'supercons'
 import Link from 'next/link'
 import { Box, Flex, Stack, Grid, Wrap, AspectRatio, SimpleGrid, Heading, HStack } from "@chakra-ui/layout"
-import { useMediaQuery } from '@chakra-ui/react'
 
 const ScheduleBoard = () => {
-    const [isLargerThan900] = useMediaQuery("(max-width: 900px)");
     return (
         <>
-            {isLargerThan900 ? (
-
-                <SimpleGrid columns={1} spacing={20} color="white" marginTop="40">
-                    <Box backgroundColor="#40434E" height="200px" borderRadius="20px" textAlign="center" padding="4">
-                        <h1>Upcoming</h1>
-                        <Box lineHeight="10px">
-                            <h4 style={{ fontWeight: "600" }}>JV | October 5, 2021 | 4:00 PM</h4>
-                            <h4 style={{ fontWeight: "600" }}>Varsity | October 7, 2021 | 4:00 PM</h4>
-                            <div className={styles.tooltip}>
-                                <button className={styles.goBtn}>
-                                    <span className={styles.tooltiptext}>Will become active once the match has started</span>
-                                    Go
-                                </button>
-                            </div>
-                            {/* <p style={{ fontWeight: "600", fontSize: "25px", paddingTop: "5px" }}> Check back soon for our schedule</p> */}
-                        </Box>
-                    </Box>
-                </SimpleGrid>
-            ) : (
-                <SimpleGrid columns={2} spacing={20} color="white" marginTop="40">
-                    <Box backgroundColor="#40434E" height="200px" borderRadius="20px" textAlign="center" padding="4" width="400px">
+                <div className={styles.wrapper}>
+                    <Box  className={styles.one} backgroundColor="#40434E" height="200px" borderRadius="20px" textAlign="center" padding="4" marginRight="auto" marginLeft="auto" width="400px">
                         <h1>Upcoming</h1>
                         <Box lineHeight="10px">
                             <h3 style={{ fontWeight: "600" }}>JV | October 5, 2021 | 4:00 PM </h3>
@@ -43,15 +22,13 @@ const ScheduleBoard = () => {
 
                         </Box>
                     </Box>
-                    <Box backgroundColor="#40434E" height="200px" borderRadius="20px" textAlign="center" padding="2">
+                    <Box className={styles.two} backgroundColor="#40434E" height="200px" borderRadius="20px" textAlign="center" padding="4" marginRight="auto" marginLeft="auto" width="400px">
                         <h1>Results</h1>
                         <Box>
                             <p style={{ fontWeight: "600", fontSize: "25px", paddingTop: "15px" }}> We don’t have any results yet! </p>
                         </Box>
                     </Box>
-                </SimpleGrid>
-            )
-            }
+                 </div>
 
         </>
     )
