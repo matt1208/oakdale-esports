16e45ac77ae19c6f484cc5fbcbdd65c6282a3eec
Matthew Van Dyke <matthewvandyke08@gmail.com>
2021-09-23 21:08:19 -0700

    Footer Input Taken Out For Style Issues

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 import Icon from 'supercons'
-import { Box, Flex, Stack, Grid, Wrap, AspectRatio, SimpleGrid, Heading, HStack } from "@chakra-ui/layout"
+import { Box, Flex, Stack, Grid, Wrap, AspectRatio, SimpleGrid, Heading, HStack, Center } from "@chakra-ui/layout"
 import { useMediaQuery } from '@chakra-ui/react'
 import OakdaleHeader from '../components/header'
 
@@ -22,20 +22,22 @@ export default function Home() {
             <main className={styles.main}>
                 <OakdaleHeader />
 
-                {isLargerThan700 ? (
 
-                <SimpleGrid columns={1} spacing={20} color="white" textAlign="center" maxWidth="700px">
-                    <Box>
+
+                <div className={styles.wrapper} style={{maxWidth: "700px", alignContent:"center", justifyContent: "center"}}>
+                    <Box className={styles.one} marginLeft="40px">
                         <h1>About Oakdale High<br></br>School Esports</h1>
                         <p>The 2021-22 esports season is the first season for Oakdale High School Esports. <br></br> Our esports program is run through <Link href="https://ccesports.org/">Central California Esports League</Link>, which provides our game play and manages our program.</p>
                         <p>Artwork by OHS teacher Matt McDonald</p>
                     </Box>
-                    <Box>
-                        <img src="OHS.png" width="369px" height="198.75" alt="OHS Artwork" />
+                    <Box className={styles.two} marginLeft="40px">
+                        <Center><img src="OHS.png" width="369px" height="198.75" alt="OHS Artwork" /></Center>
                     </Box>
 
-                    
-                    <Box marginTop="20px">
+                    <Box marginTop="40px" className={styles.fourAbout} marginLeft="40px">
+                        <Center><img src="Esports1.png" width="369px" height="198.75" style={{ borderRadius: "10px" }} alt="Black and white image of child at computer gaming." /></Center>
+                    </Box>
+                    <Box marginTop="40px" marginLeft="40px" className={styles.five}>
                         <h1>About Esports</h1>
                         <p>Esports is currently the fastest growing sport in California and opening up new opportunities for high school students.</p>
                         <HStack>
@@ -43,37 +45,7 @@ export default function Home() {
                             <Link href="/funfacts">Fun Facts About Esports</Link>
                         </HStack>
                     </Box>
-                    <Box marginTop="20px">
-                        <img src="Esports1.png" width="369px" height="198.75" style={{ borderRadius: "10px" }} alt="Black and white image of child at computer gaming." />
-                    </Box>
-                </SimpleGrid>
-
-            ) : (
-
-            <SimpleGrid columns={2} spacing={20} color="white" maxWidth="700px">
-                    <Box>
-                        <h1>About Oakdale High<br></br>School Esports</h1>
-                        <p>The 2021-22 esports season is the first season for Oakdale High School Esports. <br></br> Our esports program is run through <Link href="https://ccesports.org/">Central California Esports League</Link>, which provides our game play and manages our program.</p>
-                        <p>Artwork by OHS teacher Matt McDonald</p>
-                    </Box>
-                    <Box>
-                        <img src="OHS.png" width="369px" height="198.75" alt="OHS Artwork" />
-                    </Box>
-
-                    <Box marginTop="40px">
-                        <img src="Esports1.png" width="369px" height="198.75" style={{ borderRadius: "10px" }} alt="Black and white image of child at computer gaming." />
-                    </Box>
-                    <Box marginTop="40px" marginLeft="40px">
-                        <h1>About Esports</h1>
-                        <p>Esports is currently the fastest growing sport in California and opening up new opportunities for high school students.</p>
-                        <HStack>
-                            <Icon glyph="enter" size={40} />
-                            <Link href="/funfacts">Fun Facts About Esports</Link>
-                        </HStack>
-                    </Box>
-                </SimpleGrid>
-                )
-            }
+                </div>
 
 
 
@@ -83,7 +55,6 @@ export default function Home() {
             <footer className={styles.footer}>
                 <Link href="/streams"><p className={styles.footerLink}>Streams</p></Link>
                 <form className={styles.formFooter} action="https://gmail.us6.list-manage.com/subscribe/post?u=6c7b84c6fd110f0881bc19736&amp;id=802f764a2e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-                    <input className={styles.formInput} class="required email" id="mce-EMAIL" type="email" id="email" name="email" placeholder="EMAIL" />
                     <button className={styles.submitBtn} type="submit" id="mc-embedded-subscribe">Stay In the Loop</button>
                 </form>
                 <Link href="/about"><p className={styles.footerLink}>About</p></Link>
--- pages/streams.js
+++ pages/streams.js
@@ -75,7 +75,6 @@ export default function Home() {
             <footer className={styles.footer}>
                 <Link href="/streams"><p className={styles.footerLink}>Streams</p></Link>
                 <form className={styles.formFooter} action="https://gmail.us6.list-manage.com/subscribe/post?u=6c7b84c6fd110f0881bc19736&amp;id=802f764a2e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-                    <input className={styles.formInput} class="required email" id="mce-EMAIL" type="email" id="email" name="email" placeholder="EMAIL" />
                     <button className={styles.submitBtn} type="submit" id="mc-embedded-subscribe">Stay In the Loop</button>
                 </form>
                 <Link href="/about"><p className={styles.footerLink}>About</p></Link>
